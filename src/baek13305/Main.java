02dea4516413df6063503847b18cf11269f42f3c
SJaeYub <elapid0212@gmail.com>
2021-12-27 17:40:44 +0900

    baek13305

--- src/baek13305/Main.java
+++ src/baek13305/Main.java
@@ -7,32 +7,28 @@ public class Main {
 
     static int[] price;
     static int[] length;
-    static int n;
+    static long n;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        n = sc.nextInt();
+        n = sc.nextLong();
 
-        price = new int[n];
-        length = new int[n - 1];
+        price = new int[(int) n];
+        length = new int[(int) (n - 1)];
 
         for (int i = 0; i < n - 1; i++) {
             length[i] = sc.nextInt();
         }
 
-        int sum = 0;
         for (int i = 0; i < n; i++) {
             price[i] = sc.nextInt();
-            sum += price[i];
         }
 
-        int avg_price = sum / n;
+        ArrayList<Integer> station_list = select_gas(price[0]);
 
-        ArrayList<Integer> station_list = select_gas(avg_price);
-
-        int ans = 0;
         int curr = 0;
+        int ans = 0;
         while (curr < n) {
             int road = 0;
             int price_mid = 0;
@@ -43,30 +39,30 @@ public class Main {
                 }
                 price_mid = road * price[curr];
                 ans += price_mid;
+
                 System.out.println(ans);
                 break;
             }
 
-            Integer station_idx = station_list.remove(0);
+            Integer next_station = station_list.remove(0);
 
-            for (int i = 0; i < station_idx; i++) {
+            for (int i = curr; i < next_station; i++) {
                 road += length[i];
             }
-
             price_mid = road * price[curr];
-
             ans += price_mid;
 
-            curr = station_idx;
+            curr = next_station;
         }
     }
 
-    private static ArrayList<Integer> select_gas(int avg) {
+    private static ArrayList<Integer> select_gas(int start_price) {
         ArrayList<Integer> gas_station = new ArrayList<>();
 
         for (int i = 0; i < price.length; i++) {
-            if (price[i] < avg && i != 0 && i != n - 1) {
+            if (price[i] < start_price && i != 0 && i != n - 1) {
                 gas_station.add(i);
+                start_price = price[i];
             }
         }
 
