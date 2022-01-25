6f679eafa42c6b110c35ed4aaa0f3993874074df
SJaeYub <elapid0212@naver.com>
2022-01-25 23:43:23 +0900

    baek17780 중간 세이브

--- src/baek17780_ing/Main.java
+++ src/baek17780_ing/Main.java
@@ -7,260 +7,217 @@ import java.util.*;
 
 public class Main {
 
-    static class Piece {
-        int row;
-        int col;
+    static class Object {
+        int num;
         int dir;
-        int height;
 
-        Piece(int row, int col, int direction, int h) {
-            this.row = row;
-            this.col = col;
-            this.dir = direction;
-            this.height = h;
+        Object(int num, int dir) {
+            this.num = num;
+            this.dir = dir;
         }
     }
 
-    static int n;
-    static int k;
-    static Queue<Piece>[][] virtual;
+    static int[][] object_order;
+    static Queue<Object>[][] virtual_map;
     static int[][] map;
-    static int[] dr = {0, 0, 0, -1, 1};
-    static int[] dc = {0, 1, -1, 0, 0};
-    static Queue<Piece> q = new LinkedList<>();
-    static ArrayList<Piece> arr_list = new ArrayList<>();
+    static int n, k;
     static StringTokenizer st;
-    static boolean flag = false;
-    static int ans = 0;
+    static int[] dc = {0, 1, -1, 0, 0};
+    static int[] dr = {0, 0, 0, -1, 1};
+    static boolean ans_flag = false;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         st = new StringTokenizer(br.readLine(), " ");
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
 
-        virtual = new LinkedList[n][n];
-        map = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                virtual[i][j] = new LinkedList<>();
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
+        map = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < n; j++) {
+            for (int j = 1; j <= n; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
+        virtual_map = new LinkedList[n + 1][n + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                virtual_map[i][j] = new LinkedList<>();
+            }
+        }
+
+        object_order = new int[k + 1][2];           //기물 이동 순서를 위해
         for (int i = 0; i < k; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            int row = Integer.parseInt(st.nextToken()) - 1;
-            int col = Integer.parseInt(st.nextToken()) - 1;
+            int row = Integer.parseInt(st.nextToken());
+            int col = Integer.parseInt(st.nextToken());
             int dir = Integer.parseInt(st.nextToken());
 
-            Piece piece = new Piece(row, col, dir, 1);
-            virtual[row][col].add(piece);
-            arr_list.add(piece);
+            Object tmp = new Object(i + 1, dir);
+            virtual_map[row][col].add(tmp);
+            object_order[i + 1][0] = row;
+            object_order[i + 1][1] = col;
         }
 
-        while (!flag) {
-            ans++;
-            moving_piece();
+        for (int[] ints : object_order) {
+            System.out.println(ints[0] + " " + ints[1]);
         }
-        if (ans <= 1000) {
+
+        int ans = 0;
+        while (!ans_flag) {
+            ans++;
+            move_object();
+            if (ans >= 1000) {
+                System.out.println(-1);
+                ans_flag = true;
+            }
+        }
+        if (ans < 1000) {
             System.out.println(ans);
+        }
+
+    }
+
+    static boolean check_index(int row, int col, int num) {      //움직일 순서의 기물의 위치정보를 받아서
+
+        System.out.println(num + " " + row + " " + col + " "
+         + virtual_map[row][col].peek().dir);
+
+        if (virtual_map[row][col].peek().num == num) {              //바닥 기물 번호가 자기자신이라면
+            return true;
         } else {
-            System.out.println(-1);
+            return false;
         }
     }
 
-    static void moving_piece() {
-        q.clear();
-        q.addAll(arr_list);
+    static void move_object() {
+        for (int i = 1; i <= k; i++) {
+            if (ans_flag) {
+                break;
+            }
+            int curr_row = object_order[i][0];
+            int curr_col = object_order[i][1];
 
-        while (!q.isEmpty()) {
-            Piece curr = q.poll();
-            boolean tmp = false;
-            if (curr.height == 1) {
-                if (virtual[curr.row][curr.col].size() >= 4) {
-                    System.out.println(0);
-                    System.exit(0);
-                }
-
-                int mr = dr[curr.dir] + curr.row;
-                int mc = dc[curr.dir] + curr.col;
-
-                if (mr < 0 || mr >= n || mc < 0 || mc >= n) {
-                    tmp = true;
-                }
-
-                if (tmp) {
-
-                } else {
-                    if (map[mr][mc] == 1) {
-                        reverse_dir(curr);
-                        int new_r = dr[curr.dir] + curr.row;
-                        int new_c = dc[curr.dir] + curr.col;
-
-                        tmp = false;
-                        if (mr < 0 || mr >= n || mc < 0 || mc >= n) {
-                            tmp = true;
-                        }
-
-                        if (tmp) {
-                            System.out.println(-1);
-                            System.exit(0);
-                        } else {
-                            if (map[new_r][new_c] == 1) {
-
-                            } else if (map[new_r][new_c] == 2) {
-                                System.out.println(-1);
-                                System.exit(0);
-                            } else {
-                                move(curr, new_r, new_c, false);
-                            }
-                        }
-                    } else if (map[mr][mc] == 2) {
-                        reverse_dir(curr);
-                        int new_r = dr[curr.dir] + curr.row;
-                        int new_c = dc[curr.dir] + curr.col;
-
-                        tmp = false;
-                        if (mr < 0 || mr >= n || mc < 0 || mc >= n) {
-                            tmp = true;
-                        }
-
-                        if (tmp) {
-                            System.out.println(-1);
-                            System.exit(0);
-                        } else {
-                            if (map[new_r][new_c] == 1) {
-
-                            } else if (map[new_r][new_c] == 2) {
-                                System.out.println(-1);
-                                System.exit(0);
-                            } else {
-                                move(curr, new_r, new_c, false);
-                            }
-                        }
-
-
-                    } else {
-                        move(curr, mr, mc, false);
-                    }
-                }
-            } else {
+            if (!check_index(curr_row, curr_col, i)) {
                 continue;
             }
 
+            Object curr = virtual_map[curr_row][curr_col].peek();
+
+            int next_row = curr_row + dr[curr.dir];
+            int next_col = curr_col + dc[curr.dir];
+            int next_box_col;
+
+            if (next_row < 1 || next_row > n || next_col < 1 || next_col > n) {             //벗어나느 ㄴ경우
+                change_dir(curr_row, curr_col);             //방향을 바꿔주고
+
+                next_row = curr_row + dr[curr.dir];
+                next_col = curr_col + dc[curr.dir];
+                next_box_col = map[next_row][next_col];
+
+                if (next_box_col == 0) {                //흰
+                    move(curr_row, curr_col, next_row, next_col);
+                } else if (next_box_col == 1) {             //빨
+                    move(curr_row, curr_col, next_row, next_col);
+
+                    reverse_index(next_row, next_col);
+
+               } else {                                    //파
+                    continue;
+                }
+
+            } else {        //벗어나지 않는 경우에
+                next_box_col = map[next_row][next_col];
+                if (next_box_col == 0) {                //흰
+                    move(curr_row, curr_col, next_row, next_col);
+
+                } else if (next_box_col == 1) {             //빨
+                    move(curr_row, curr_col, next_row, next_col);
+
+                    reverse_index(next_row, next_col);
+
+                } else {                                    //파
+                    change_dir(curr_row, curr_col);             //방향을 바꿔주고
+
+                    next_row = curr_row + dr[curr.dir];
+                    next_col = curr_col + dc[curr.dir];
+
+                    if (next_row < 1 || next_row > n || next_col < 1 || next_col > n) {
+                        continue;
+                    } else {
+                        next_box_col = map[next_row][next_col];
+                        if (next_box_col == 0) {                //흰
+                            move(curr_row, curr_col, next_row, next_col);
+
+                        } else if (next_box_col == 1) {             //빨
+                            move(curr_row, curr_col, next_row, next_col);
+
+                            reverse_index(next_row, next_col);
+
+                        } else {                                    //파
+                            continue;
+                        }
+                    }
+                }
+            }
+            System.out.println(curr.num + " " + next_row + " " + next_col
+            + " " + virtual_map[next_row][next_col].peek().dir);
+        }
+        return;
+    }
+
+    static void reverse_index(int row, int col) {
+        Object[] tmp_arr = new Object[virtual_map[row][col].size()];
+        int idx = 0;
+
+        while (!virtual_map[row][col].isEmpty()) {
+            tmp_arr[idx] = virtual_map[row][col].poll();
+            idx++;
+        }
+
+        for (int i = tmp_arr.length - 1; i >= 0; i--) {
+            virtual_map[row][col].add(tmp_arr[i]);
+        }
+
+    }
+
+    static void change_dir(int row, int col) {
+        int curr_dir = virtual_map[row][col].peek().dir;
+        if (curr_dir == 1) {
+            virtual_map[row][col].peek().dir = 2;
+        } else if (curr_dir == 2) {
+            virtual_map[row][col].peek().dir = 1;
+        } else if (curr_dir == 3) {
+            virtual_map[row][col].peek().dir = 4;
+        } else {
+            virtual_map[row][col].peek().dir = 3;
         }
     }
 
-    //    static void moving_piece() {
-//        q.clear();
-//        q.addAll(arr_list);             //1~k번까지 기물을 큐에 새로 담아준다
-//        while (!q.isEmpty()) {
-//            boolean tmp = false;
-//            Piece curr = q.poll();
-//            if (curr.height == 1) {     //현재 기물이 바닥인지 아닌지
-//                int mr = dr[curr.dir] + curr.row;
-//                int mc = dc[curr.dir] + curr.col;
-//
-//                if (mr < 0 || mr >= n || mc < 0 || mc >= n) {           //맵을 벗어난다면
-//                    tmp = true;
-//                }
-//
-//                if (tmp) {
-//                    reverse_dir(curr);                                      //방향을 바꾸고
-//                    int new_r = dr[curr.dir] + curr.row;
-//                    int new_c = dc[curr.dir] + curr.col;
-//
-//                    if (map[new_r][new_c] == 1) {
-//                        move(curr, new_r, new_c, true);                 //이동 시키고 위치 인덱스도 바까주고
-//                    } else if (map[new_r][new_c] == 2) {
-//                        //파랑이면 아무것도 안함
-//                    } else if (map[new_r][new_c] == 0) {
-//                        move(curr, new_r, new_c, false);            //기존 위치의 모든 기물을 이동시킨다
-//                    }
-//
-//                } else {                                    //아니라면
-//
-//                    if (map[mr][mc] == 1) {                 //이동 칸이 빨강
-//                        move(curr, mr, mc, true);                 //이동 시키고 위치 인덱스도 바까주고
-//                    } else if (map[mr][mc] == 2) {          //이동 칸이 파랑
-//                        reverse_dir(curr);                  //방향 뒤집어 주고
-//
-//                        int new_r = dr[curr.dir] + curr.row;    //바뀐 방향의 이동할 타일 색
-//                        int new_c = dc[curr.dir] + curr.col;
-//
-//                        tmp = false;            //tmp 초기화
-//                        if (new_r < 0 || new_r >= n || new_c < 0 || new_c >= n) {
-//                            tmp = true;
-//                        }
-//
-//                        if (tmp) {           //맵을 벗어나면
-//                            //바뀐 방향 유지한채 끝
-//                        } else {                  //맵을 벗어나지 않는다면
-//                            if (map[new_r][new_c] == 1) {
-//                                move(curr, new_r, new_c, true);                 //이동 시키고 위치 인덱스도 바까주고
-//                            } else if (map[new_r][new_c] == 2) {
-//                                //파랑이면 아무것도 안함
-//                            } else if (map[new_r][new_c] == 0) {
-//                                move(curr, new_r, new_c, false);            //기존 위치의 모든 기물을 이동시킨다
-//                            }
-//                        }
-//                    } else {                                //이동 칸이 흰
-//                        move(curr, mr, mc, false);                 //기존 위치의 모든 기물을 이동시킨다
-//                    }
-//                }
-//            } else {            //위에 얹혀있는 기물이면 스킵
-//                continue;
-//            }
-//        }
-//    }
-//
-    static void move(Piece piece, int mr, int mc, boolean red_check) {
-        int next_height = virtual[mr][mc].size();
-        for (Piece temp : virtual[mr][mc]) {
-            temp.height += next_height;
-            virtual[mr][mc].add(temp);
-            if (virtual[mr][mc].size() >= 4) {
-                flag = true;
+    static void move(int row, int col, int next_r, int next_c) {
+        while (!virtual_map[row][col].isEmpty()) {
+            Object curr = virtual_map[row][col].poll();         //기존꺼에서 뽑아다
+
+            virtual_map[next_r][next_c].add(curr);              //새거에다 넣어주고
+            object_order[curr.num][0] = next_r;
+            object_order[curr.num][1] = next_c;                 //위치 정보 업데이트
+
+            if (virtual_map[next_r][next_c].size() >= 4) {
+                ans_flag = true;
                 return;
             }
         }
     }
 
-    //
-//    static void reverse_height(ArrayList<Piece> arr_tmp, int start) {
-//        int cnt = 0;
-//        for (int i = arr_tmp.size() - 1; i >= 0; i--) {
-//            arr_tmp.get(i).height = start + cnt;
-//            cnt++;
-//        }
-//    }
-//
-    static void reverse_dir(Piece piece) {
-        if (piece.dir == 1) {                //기존 방향에 맞춰 뒤집어준다
-            piece.dir = 2;
-        } else if (piece.dir == 2) {
-            piece.dir = 1;
-        } else if (piece.dir == 3) {
-            piece.dir = 4;
-        } else {
-            piece.dir = 3;
-        }
-    }
-
-    private static void print_map(int[][] map) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(map[i][j]);
+    private static void print_vir_map() {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(virtual_map[i][j].size() + " ");
             }
             System.out.println();
         }
     }
+
 }
